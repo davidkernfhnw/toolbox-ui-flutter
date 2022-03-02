5dd552a94ca95e30e066cae256ac43f4c67fc7d5
davidkernfhnw <david.kern@fhnw.ch>
2022-03-02 08:26:49 +0100

    translation text for settings module

--- lib/app/translation/app_translation.dart
+++ lib/app/translation/app_translation.dart
@@ -8,34 +8,213 @@ abstract class AppTranslation {
 }
 
 final Map<String, String> en = {
+
+  /* module: termsAndConditions */
+  "terms-conditions":                   "Terms and Conditions",
+  "at-least-16":                        "I am at least 16 years old.",
+  "have-signed-consent-form":           "I have signed a consent form.",
+  "read-agree-privacy-policy":          "I have read and agree with the Privacy Policy of the GEIGER Toolbox.",
+  "title-company-does":                 "Your company does...",
+  "label1-company-does":                "only consume digital products",
+  "label2-company-does":                "sell digital products but not develop them",
+  "label3-company-does":                "develop and sell digital products itself",
+  "info-accept-terms":                  "In order to use the toolbox all terms above have to be accepted",
+  "continue-btn-label":                 "Continue",
+
+
+  /* module: settings */
+  "title-settings":                     "Settings",
+  "profile":                            "Profile",
+  "data-protection":                    "Data Protection",
+  "data":                               "Data",
+
+  /* module: settings -> widgets: data_protection_view */
+  "warning":                            "Warning",
+  "data-access-warning-text":           "Are you sure you want to disable Data Access and Processing? The setting will be applied after you restart the app.",
+  "snackbar-data-access-title":         "Data Access",
+  "snackbar-data-access-success-text":  "Consent successfully updated.",
+  "snackbar-data-access-failed-text":   "Consent fail to update.",
+
+  "ok-btn-label":                       "ok",
+  "cancel-btn-label":                   "cancel",
+
+  "data-access-processing-title":       "Data Access and Processing",
+  "data-access-processing-desc":        "Allow the toolbox to calculate your risk score and receive protection recommendations.",
+
+  "data-sharing-title":                 "Data Sharing",
+
+  "data-sharing-label1":                "Do not share",
+  "data-sharing-desc1":                 "Data will remain on this single device only.",
+  "data-sharing-dialog1":               "Are you sure you want to stop your data from be replicated. The setting will be applied after you restart the app.",
+
+  "data-sharing-label2":                "Replicate securely between your devices.",
+  "data-sharing-desc2":                 "All your data will be available on all your devices.",
+
+  "snackbar-replication-title":         "Replication",
+  "snackbar-replication-success-text":  "Your data was successfully replicated.",
+  "snackbar-replication-failed-text":   "Your data replication failed.",
+
+  "data-sharing-label3":                "Share data with GEIGER cloud.",
+  "data-sharing-desc3":                 "Your tools’ data will be used to improve GEIGER over time.",
+
+  "data-sharing-label4":                "Tools may process  your data",
+  "data-sharing-desc4":                 "This setting enables or disables the tools’ ability to process your data and offer personalised services. When enabled, you can disable the setting for each tool.",
+  "tools-btn-label":                    "Tools",
+
+  "data-sharing-label5":                "Incident reporting",
+  "data-sharing-desc5":                 "When experiencing an incident, you will have the ability to submit an incident report to your chosen cybersecurity agency (CERT). You will be asked each time whether you want to report the incident.",
+
+  "alert":                              "Alert",
+  "info-alert-needs-permission":        "Toolbox needs you to grant permission for data access and processing.",
+
+  "nav-to-scan-screen-btn-label":       "Navigate to Scan Screen",
+
+  /* module: settings -> widgets: data_view */
+  "allow-data-access-process":          "Allow Data Access and Process!",
+  "export-btn-label":                   "Export",
+
+  /* module: settings -> widgets: profile_view */
+  "user-name-label":                    "User Name",
+  "user-name-hint":                     "Enter User name",
+  "user-name-warning":                  "Please Enter your Name",
+  "device-name-label":                  "Name of this Device",
+  "device-name-hint":                   "Enter Device name",
+  "device-name-warning":                "Please Enter your Device Name",
+  "company-owner-label":                "I’m a company owner",
+  "company-owner-desc":                 "As an owner you can compare your company’s geiger score with others.",
+
+  "language":                           "Language",
+  "country":                            "Country",
+  "country-hint":                       "Select Your Country",
+  "competent-cert":                     "Competent CERT",
+  "competent-cert-hint":                "Select Competent CERT",
+  "profession-association":             "Profession Association",
+  "profession-association-hint":        "Select Profession Association",
+
+  "snackbar-update-success-title":      "Success",
+  "snackbar-update-success-desc":       "Updated Successfully.",
+
+  "snackbar-update-failed-title":       "Message Alert",
+  "snackbar-update-failed-desc":        "Updated Failed! Contact the Developer",
+
+  "update-btn-label":                   "Update",
+
+
+
+  /* module: home */
   "geiger-toolbox": "Geiger Toolbox",
-  "update-button-label": "Update",
-  "title-settings": "Settings",
-  "profile": "Profile",
-  "data-protection": "Data Protection",
-  "data": "Data"
+  "no-data-found": "NO DATA FOUND",
 };
 final Map<String, String> deCh = {
-  "geiger-toolbox": "Geiger Werkzeugkasten",
-  "update-button-label": "Aktualisieren",
-  "title-settings": "Einstellungen",
-  "profile": "Profil",
-  "data-protection": "Datenschutz",
-  "data": "Daten"
+  /* module: termsAndConditions */
+  "terms-conditions":                   "Allgemeine Geschäftsbedingungen",
+  "at-least-16":                        "Ich bin mindestens 16 Jahre alt.",
+  "have-signed-consent-form":           "Ich habe eine Einverständniserklärung unterschrieben.",
+  "read-agree-privacy-policy":          "Ich habe die Datenschutzbestimmungen der GEIGER-Toolbox gelesen und bin damit einverstanden.",
+  "title-company-does":                 "Ihr Unternehmen...",
+  "label1-company-does":                "nutzt nur digitale Produkte",
+  "label2-company-does":                "verkauft digital Produkte, aber entwickelt selbst keine",
+  "label3-company-does":                "entwickelt und verkauft digitale Produkte",
+  "info-accept-terms":                  "Für die Nutzung der Toolbox, müssen alle oben genannten Bedingungen akzeptiert werden",
+  "continue-btn-label":                 "Weiter",
+
+
+  /* module: settings */
+  "title-settings":                     "Einstellungen",
+  "profile":                            "Profil",
+  "data-protection":                    "Datenschutz",
+  "data":                               "Daten",
+
+  /* module: settings -> widgets: data_protection_view */
+  "warning":                            "Warnung",
+  "data-access-warning-text":           "Sind Sie sicher, dass Sie den Datenzugriff und die Datenverarbeitung deaktivieren möchten? Die Einstellung wird nach dem Neustart der App übernommen.",
+  "snackbar-data-access-title":         "Datenzugriff",
+  "snackbar-data-access-success-text":  "Genehmigungen erfoglreich aktualisiert.",
+  "snackbar-data-access-failed-text":   "Genehmigungen konnten nicht aktualisiert werden.",
+
+  "ok-btn-label":                       "Okay",
+  "cancel-btn-label":                   "Abbrechen",
+
+  "data-access-processing-title":       "Datenzugriff und -verarbeitung",
+  "data-access-processing-desc":        "Erlauben Sie der Toolbox, Ihren Risiko-Score zu berechnen, sowie Sicherheitsempfehlungen zu erhalten.",
+
+  "data-sharing-title":                 "Datenfreigabe",
+
+  "data-sharing-label1":                "nicht freigeben",
+  "data-sharing-desc1":                 "Die Daten verbleiben nur auf diesem Gerät.",
+  "data-sharing-dialog1":               "Sind Sie sicher, dass Sie die Abbildung Ihrer Daten unterbinden möchten. Die Einstellung wird nach dem Neustart der App übernommen.",
+
+  "data-sharing-label2":                "Sichere Abbildung zwischen Ihren Geräten.",
+  "data-sharing-desc2":                 "Die Daten sind auf allen Geräten verfügbar.",
+
+  "snackbar-replication-title":         "Abbildung",
+  "snackbar-replication-success-text":  "Die Daten wurden erfolgreich abgebildet.",
+  "snackbar-replication-failed-text":   "Die Abbildung der Daten ist fehlgeschlagen.",
+
+  "data-sharing-label3":                "Daten mit der GEIGER-Cloud teilen.",
+  "data-sharing-desc3":                 "Ihre Tools Daten werden verwendet, um GEIGER im Laufe der Zeit zu verbessern.",
+
+  "data-sharing-label4":                "Tools können Ihre Daten verarbeiten",
+  "data-sharing-desc4":                 "Diese Einstellung aktiviert oder deaktiviert die Berechtigung der Tools, Ihre Daten zu verarbeiten und personalisierte Dienste bereitzustellen. Wenn sie aktiviert ist, kann jedes Tool individuell deaktiviert werden.",
+  "tools-btn-label":                    "Werkzeuge",
+
+  "data-sharing-label5":                "Vorfall melden",
+  "data-sharing-desc5":                 "Bei einem Vorfall haben Sie die Möglichkeit, eine Vorfallbericht an die von Ihnen gewählte Cybersicherheitsagentur (CERT) zu senden. Sie werden jedes Mal gefragt, ob Sie den Vorfall melden möchten.",
+
+  "alert":                              "Warnung",
+  "info-alert-needs-permission":        "Die Toolbox benötigt Ihre Zustimmung für den Datenzugriff und die Datenverarbeitung.",
+
+  "nav-to-scan-screen-btn-label":       "Zum Scan-Bildschirm",
+
+  /* module: settings -> widgets: data_view */
+  "allow-data-access-process":          "Datenzugriff und Verarbeitung zulassen!",
+  "export-btn-label":                   "Exportieren",
+
+  /* module: settings -> widgets: profile_view */
+  "user-name-label":                    "Nutzername",
+  "user-name-hint":                     "Nutzername eingeben",
+  "user-name-warning":                  "Bitte Nutzername eingeben",
+  "device-name-label":                  "Name dieses Geräts",
+  "device-name-hint":                   "Gerätenamen eingeben",
+  "device-name-warning":                "Bitte Gerätenamen eingeben",
+  "company-owner-label":                "Ich bin ein Inhaber eines Unternehmens",
+  "company-owner-desc":                 "Als Eigentümer können Sie den Geiger-Score Ihres Unternehmens mit andereren Unternehmen vergleichen.",
+  "language":                           "Sprache",
+  "country":                            "Land",
+  "country-hint":                       "Land auswählen",
+  "competent-cert":                     "Zuständiges CERT",
+  "competent-cert-hint":                "Zuständiges CERT auswählen",
+  "profession-association":             "Berufsverband",
+  "profession-association-hint":        "Berufsverband auswählen",
+
+  "snackbar-update-success-title":      "Erfolgreich",
+  "snackbar-update-success-desc":       "erfolgreich aktualisiert.",
+
+  "snackbar-update-failed-title":       "Warnmeldung",
+  "snackbar-update-failed-desc":        "Aktualisierung fehlgeschlagen! Kontaktieren Sie den Entwickler.",
+
+  "update-btn-label":                   "Aktualisieren",
+
+
+
+  /* module: home */
+  "geiger-toolbox": "Geiger Toolbox",
+  "no-data-found": "KEINE DATEN GEFUNDEN",
+
 };
 final Map<String, String> ro = {
-  "geiger-toolbox": "Geiger Toolbox",
+  /*"geiger-toolbox": "Geiger Toolbox",
   "update-button-label": "Actualizați",
   "title-settings": "setări",
   "profile": "Profil",
   "data-protection": "Protejarea datelor",
-  "data": "Date"
+  "data": "Date"*/
 };
 final Map<String, String> nlNl = {
-  "geiger-toolbox": "Geiger-gereedschapskist",
+  /*"geiger-toolbox": "Geiger-gereedschapskist",
   "update-button-label": "Bijwerken",
   "title-settings": "instellingen",
   "profile": "Profiel",
   "data-protection": "Gegevensbescherming",
-  "data": "Gegevens"
+  "data": "Gegevens"*/
 };
